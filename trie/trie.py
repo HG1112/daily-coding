6b482b4c64c949a0f19bf6ba2d4be290a18a9a75
harry <opensourcebyharry@gmail.com>
2023-09-02 15:53:11 -0500

    trie impl python

---
+++ trie/trie.py
@@ -0,0 +1,37 @@
+class TrieNode:
+
+    def __init__(self) -> None:
+        self.end = False
+        self.child = [None for _ in range(26)]
+
+class Trie:
+
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cursor = self.root
+        for c in word:
+            idx = ord(c) - ord('a')
+            if not cursor.child[idx]:
+                cursor.child = TrieNode()
+            cursor = cursor.child[idx]
+        cursor.end = True
+
+    def startsWith(self, pre: str) -> None:
+        cursor = self.root
+        for c in pre:
+            idx = ord(c) - ord('a')
+            if not cursor.child[idx]:
+                return False
+            cursor = cursor.child[idx]
+        return True
+
+    def contains(self, word: str) -> None:
+        cursor = self.root
+        for c in word:
+            idx = ord(c) - ord('a')
+            if not cursor.child[idx]:
+                return False
+            cursor = cursor.child[idx]
+        return cursor.end
