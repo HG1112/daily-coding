96de2e2d85271300d3c63ce7d475100950045194
harry <opensourcebyharry@gmail.com>
2023-09-02 15:21:04 -0500

    coin change I

---
+++ coin-change/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    # Problem 1 
+    def min_coins(self, coins: list[int], amount: int) -> int:
+        dp = [-1 for _ in range(amount+1)]
+        dp[0] = 0
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i >= coin and dp[i-coin] > 0:
+                    if dp[i] == -1:
+                        dp[i] = dp[i-coin]+1
+                    else:
+                        dp[i] = min(dp[i-coin]+1, dp[i])
+        return dp[amount]
+
