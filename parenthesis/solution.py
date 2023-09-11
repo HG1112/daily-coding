c664d3c38017100792cd74e444495d67bf489a9c
harry <opensourcebyharry@gmail.com>
2023-09-11 11:56:29 -0500

    LC 20: Valid Parenthesis

---
+++ parenthesis/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    # Problem 1 
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c == '{' or c == '[' or c == '(':
+                stack.append(c)
+            elif stack:
+                if c == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif c == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif c == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+            else:
+                return False
+        return not stack
