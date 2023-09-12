05ea08399a06bbe46d447369f2f6004ad4d6c6b3
harry <opensourcebyharry@gmail.com>
2023-09-12 15:06:50 -0500

    LC 33: Search in rotated array

--- binary-search/Solution.java
+++ binary-search/Solution.java
@@ -61,4 +61,34 @@ class Solution {
     }
     return -1;
   }
+
+  // Problem 4
+  public int search(int[] nums, int target) {
+    int n = nums.length;
+    int lo = 0, hi = n-1;
+    while (lo <= hi) {
+      int mid = lo + ((hi - lo) >> 1);
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[lo] < nums[hi]) {
+        if (nums[mid] < target)
+          lo = mid+1;
+        else 
+          hi = mid-1;
+      } else {
+        if (nums[mid] < nums[hi]) {
+          if (nums[mid] < target && target <= nums[hi])
+            lo = mid+1;
+          else 
+            hi = mid-1;
+        } else {
+          if (nums[lo] <= target && target < nums[mid])
+            hi = mid-1;
+          else 
+            lo = mid+1;
+        }
+      }
+    }
+    return -1;
+  }
 }
--- binary-search/solution.py
+++ binary-search/solution.py
@@ -57,4 +57,30 @@ class Solution:
                 return nums[mid]
         return -1
 
+    # Problem 4
+    def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums)-1
+        while lo <= hi:
+            mid = lo + ((hi - lo) >> 1)
+            if nums[mid] == target:
+                return mid
+            elif nums[lo] < nums[hi]:
+                if nums[mid] < target:
+                    lo = mid+1
+                else:
+                    hi = mid-1
+            else:
+                if nums[mid] < nums[hi]:
+                    if nums[mid] < target and target <= nums[hi]:
+                        lo = mid+1
+                    else:
+                        hi = mid-1
+                else:
+                    if nums[lo] <= target and target < nums[mid]:
+                        hi = mid-1
+                    else:
+                        lo = mid+1
+        return -1
+
 
